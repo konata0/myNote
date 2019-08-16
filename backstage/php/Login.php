7e81f99ea03a776f596cc48fbb18d4d8af003183
konata <xyhkonata@gmail.com>
2019-08-16 23:40:21 +0800

    PHP密码验证，logo修改

--- backstage/php/Login.php
+++ backstage/php/Login.php
@@ -1,11 +1,26 @@
 ﻿<?php
+
+	$password_validation = "123456";
+
+	$post = json_decode(file_get_contents('php://input'), true);
+	$password = $post["password"];
+	
 	$data = array(
 		"token" => "0123456789"
 	);
-	$re = array(
-		"code" => 0,
-		"data" => $data
-	);
+	
+	if($password == $password_validation){
+		$re = array(
+			"code" => 0,
+			"data" => $data
+		);
+	}else{
+		$re = array(
+			"code" => -1
+		);
+	}
+	
+	
 	header('Content-Type:application/json; charset=utf-8');
 	exit(json_encode($re,JSON_UNESCAPED_UNICODE));
 ?>
